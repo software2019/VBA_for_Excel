2c95a98c7fedf44e8b55892bd1eec16f79bac289
Shidur Rahman <md.rahman@plymouth.ac.uk>
2022-11-21 18:16:51 +0000

    new file

---
+++ rowsMovetoSheets.vb
@@ -0,0 +1,11 @@
+Sub MoveTodouble_MVM_PT()
+    Dim xRg As Range
+    Dim xCell As Range
+    Dim A As Long
+    Dim B As Long
+    Dim I As Long
+    A = Worksheets("master").UsedRange.Rows.Count
+    B = Worksheets("double_MVM_PT").UsedRange.Rows.Count
+    
+    
+End Sub
